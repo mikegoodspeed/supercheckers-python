d757e98b62f4d1097a10719038ba9dafab23194e
Mike Goodspeed <mikegoodspeed@users.noreply.github.com>
2019-12-02 12:31:34 -0700

    use pathlib

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
-import os
+from pathlib import Path
 from typing import Any, Dict
 
 from setuptools import find_packages, setup
 
 PACKAGE = "supercheckers"
-HERE = os.path.abspath(os.path.dirname(__file__))
+HERE = Path(__file__).parent
 
 
-def read(file_name: str) -> str:
-    with open(os.path.join(HERE, file_name)) as f:
+def read(path: Path) -> str:
+    with open(HERE / path) as f:
         return f.read()
 
 
 meta: Dict[str, Any] = {}
-exec(read(os.path.join(PACKAGE, "__meta__.py")), {}, meta)
+exec(read(Path(PACKAGE) / "__meta__.py"), {}, meta)
 
 setup(
     name=PACKAGE,
     description=meta["__description__"],
-    long_description=read("README.md"),
+    long_description=read(Path("README.md")),
     long_description_content_type="text/markdown",
     author=meta["__author__"],
     version=meta["__version__"],
